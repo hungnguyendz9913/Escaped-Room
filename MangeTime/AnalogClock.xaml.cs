c712792342794f82514589fcc8038dcbc36f393a
phongthanh1412 <phongthanhnguyen1879@gmail.com>
2025-10-02 17:24:32 +0700

    New: add time control function to count down

---
+++ MangeTime/AnalogClock.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace EscapeRoom.ManageTime
+{
+    public sealed partial class AnalogClock : UserControl
+    {
+        private readonly DispatcherTimer _timer;
+        private int _countdownSeconds;
+
+        public AnalogClock()
+        {
+            this.InitializeComponent();
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            _timer.Tick += Timer_Tick;
+        }
+
+        
+        public void SetCountdown(int seconds)
+        {
+            _countdownSeconds = seconds;
+
+            if (_countdownSeconds > 0)
+            {
+                CountdownText.Text = _countdownSeconds.ToString();
+                _timer.Start();
+            }
+            else
+            {
+                _timer.Stop();
+            }
+        }
+
+        private void Timer_Tick(object? sender, object e)
+        {
+            if (_countdownSeconds > 0)
+            {
+                _countdownSeconds--;
+                CountdownText.Text = _countdownSeconds.ToString();
+            }
+            else
+            {
+                _timer.Stop();
+                // Optional: trigger "time-up" effect here
+                CountdownText.Text = "0";
+            }
+        }
+    }
+}
--- Views/RoomWindow.xaml.cs
+++ Views/RoomWindow.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using EscapeRoom.Data;
+using EscapeRoom.ManageTime;
 using EscapeRoom.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 using System;
+using System.Linq;
 
 namespace EscapeRoom.Views
 {
     public sealed partial class RoomWindow : Page
     {
         private int _index;
+        private int _timeLeft;
+        private DispatcherTimer? _timer;
 
         public RoomWindow()
         {
@@ -22,41 +26,77 @@ namespace EscapeRoom.Views
             base.OnNavigatedTo(e);
             _index = e.Parameter is int i ? i : 0;
             LoadPuzzle();
+            StartCountdown();
         }
 
         private void LoadPuzzle()
         {
             var p = PuzzleRepo.Puzzles[_index];
-            PuzzleImage.Source = new BitmapImage(new Uri($"ms-appx:///{p.ImagePath}"));
-            PuzzleText.Text = p.Question;
-            BtnA.Content = p.Options[0];
-            BtnB.Content = p.Options[1];
-            BtnC.Content = p.Options[2];
+
+            PuzzleImage.Source = new BitmapImage(new Uri($"ms-appx:///{p.ImagePath ?? ""}"));
+            PuzzleText.Text = p.Question ?? "";
+
+            BtnAText.Text = p.Options.ElementAtOrDefault(0) ?? "";
+            BtnBText.Text = p.Options.ElementAtOrDefault(1) ?? "";
+            BtnCText.Text = p.Options.ElementAtOrDefault(2) ?? "";
+        }
+
+        private void StartCountdown()
+        {
+            _timeLeft = 60;
+            AnalogClockControl.SetCountdown(_timeLeft);
+
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, object e)
+        {
+            _timeLeft--;
+
+            if (_timeLeft >= 0)
+                AnalogClockControl.SetCountdown(_timeLeft);
+
+            if (_timeLeft <= 0)
+            {
+                _timer?.Stop();
+                TimeUp();
+            }
+        }
+
+        private void TimeUp()
+        {
+            Frame?.Navigate(typeof(EndWindow));
         }
 
         private void AnswerA_Click(object s, RoutedEventArgs e) => SelectAnswer("Abcdef");
         private void AnswerB_Click(object s, RoutedEventArgs e) => SelectAnswer("Bcdefg");
         private void AnswerC_Click(object s, RoutedEventArgs e) => SelectAnswer("Cdefgh");
 
-        private async void SelectAnswer(string choiceIndex)
+        private void SelectAnswer(string choiceIndex)
         {
+            _timer?.Stop();
+
             var p = PuzzleRepo.Puzzles[_index];
-            bool correct = Sha256Hasher.VerifyHex(choiceIndex, p.CorrectIndex);
+            if (p == null)
+            {
+                Frame?.Navigate(typeof(EndWindow));
+                return;
+            }
+
+            bool correct = Sha256Hasher.VerifyHex(choiceIndex, p.CorrectIndex ?? "");
 
             if (!correct)
             {
-                Frame.Navigate(typeof(EndWindow));
+                Frame?.Navigate(typeof(EndWindow));
             }
             else
             {
                 if (_index + 1 < PuzzleRepo.Puzzles.Count)
-                {
-                    Frame.Navigate(typeof(RoomWindow), _index + 1);
-                }
+                    Frame?.Navigate(typeof(RoomWindow), _index + 1);
                 else
-                {
-                    Frame.Navigate(typeof(WinnerWindow));
-                }
+                    Frame?.Navigate(typeof(WinnerWindow));
             }
         }
     }
